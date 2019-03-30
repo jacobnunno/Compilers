d6566b4c1633e03ed8d500896c0e5a465ded052d
jacobnunno <jacobnunno@hotmail.com>
2019-03-30 16:31:35 -0400

    new master

---
+++ absyn/OpExp.java
@@ -0,0 +1,33 @@
+package absyn;
+
+public class OpExp extends Exp {
+  public final static int PLUS  = 0;
+  public final static int MINUS = 1;
+  public final static int MUL = 2;
+  public final static int DIV = 3;
+  public final static int EQ = 4;
+  public final static int NE = 5;
+  public final static int LT = 6;
+  public final static int LE = 7;
+  public final static int GT = 8;
+  public final static int GE = 9;
+
+  public Exp left;
+  public int op;
+  public Exp right;
+
+  public OpExp( int row, int col, Exp left, int op, Exp right ) {
+    this.row = row;
+    this.col = col;
+    this.left = left;
+    this.op = op;
+    this.right = right;
+  }
+
+  public void accept( AbsynVisitor visitor, int level ) {
+    visitor.visit( this, level );
+  }
+   public void accept( SemanticAnalyzerBuilder builder, int level ) {
+    builder.build( this, level );
+  }
+}
